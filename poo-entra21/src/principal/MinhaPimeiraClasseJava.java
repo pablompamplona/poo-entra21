4066e5804e36ecaa6a9f2788279412728fe826e1
User <pablompamplona@gmail.com>
2022-06-22 21:09:43 -0300

    Primeiro commit pela IDE

---
+++ poo-entra21/src/principal/MinhaPimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package principal;
+
+public class MinhaPimeiraClasseJava {
+
+	public static void main(String[] args) {
+		String texto = "turma";
+		System.out.println("Ola " + texto + "!");
+	}
+
+}
